0b0cb33701141b90345f12b2f22e1844aa51823b
Carlos <almendracarloshenrique@gmail.com>
2025-04-06 19:44:49 -0300

    add h2 console + update no init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
-     uuid TEXT PRIMARY KEY,
-     name TEXT NOT NULL,
-     email TEXT NOT NULL,
-     password TEXT NOT NULL
-);
+    uuid VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+    );
